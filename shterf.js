1f3bb288c353cd001c668a17217b244a21eb9c06
Louisa <louisa@millerout.com>
2018-07-18 11:08:06 -0400

    bleg

--- shterf.js
+++ shterf.js
@@ -7,5 +7,11 @@
 
     alert(message);
     console.log(message);
-
+var value1 = 10;
+var value2 = 15;
+function multiply(v1, v2){
+    var multiplied = v1 * v2;
+    alert(multiplied);
+}
+multiply(value1, value2);
 </script>
